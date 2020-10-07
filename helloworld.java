7e8fb2418f163deacbf2bc03736fcc5941aab659
Sudhir <sudhirgnc@gmail.com>
2020-10-07 14:47:32 +0000

    Added one line in helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,9 +1,11 @@
 //This will print a message
 public class helloworld
 {
+
 	public static void main(String[] args)
 	{
 		system.out.println("Hello")
+		system.out.println("Welcome to github")
 	}
 }
 
